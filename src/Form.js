9b13716c3eeaed980c7dd862f18271b678a0f999
Samira Ghorbanzadeh <samiraqorbanzade@gmail.com>
2023-07-04 23:15:42 +0330

    removing the warnings by editing the code

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,3 @@
-import React from "react";
-import axios from "axios";
-
 import "./Form.css";
 
 export default function Form() {
--- src/Information.js
+++ src/Information.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 import "./Information.css";
 
 export default function Information() {
