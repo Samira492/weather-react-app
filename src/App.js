c905ad308f418b618ed7d6929212b015d6e4a833
Samira Ghorbanzadeh <samiraqorbanzade@gmail.com>
2023-07-06 01:29:07 +0330

    changing the structure of the html+ removing previous design

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import Form from "./Form";
-import Description from "./Description";
-import Information from "./Information";
-import Forecast from "./Forecast";
-import Footer from "./Footer";
+import Weather from "./Weather";
 
 import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <div className="App">
-        <Form />
-        <Information />
-        <Description />
-        <Forecast />
+    <div className="App">
+      <div className="container">
+        <Weather />
+
+        <footer>
+          This project was coded by Samira Ghorbanzadeh and is {""}
+          <a
+            href="https://github.com/Samira492/weather-react-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
-      <Footer />
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              class="form-control"
+              autoComplete="off"
+              autoFocus
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wed 07:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="Cloudy-weather"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 32%</li>
+            <li>Wind: 3 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
