d047bc142e6e66a0113656926cd369b3ea59194a
Samira Ghorbanzadeh <samiraqorbanzade@gmail.com>
2023-07-07 01:30:10 +0330

    use the correct icon instead of the fake one

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       city: response.data.name,
     });
   }
