cda0bd2e56103b894f2e4880a2b904b456df7624
Samira Ghorbanzadeh <samiraqorbanzade@gmail.com>
2023-07-07 04:19:27 +0330

    changing the color of icons

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="purple"
+      color="#0B5ED7"
       size={52}
       animate={true}
     />
